1f41e43f29747cbe980413361b78d9df31078f61
RTha66 <66070175@kmitl.ac.th>
2025-02-07 19:21:15 +0700

    Update background.js

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,13 +1,4 @@
-document.getElementById("changeColorBtn").addEventListener("click", function() {
-    const randomColor = getRandomColor();
+document.getElementById("colorButton").addEventListener("click", function() {
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 });
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
